aaac4ccdefa1c6f08f73dc3eca938a80c55b5df9
Brian-Tas <brianjtascone@gmail.com>
2024-11-15 11:50:53 -0500

    Update Neuron.js

--- NeuralNetwork/Neuron.js
+++ NeuralNetwork/Neuron.js
@@ -1,1 +1,20 @@
+class Neuron {
+    constructor(id, type, value=0) {
+      if(typeof id === 'undefined') {
+        console.error('No id provided for Neuron construction');
+        process.exit(1);
+      }
 
+      if(typeof type === 'undefined') {
+        console.error('No type specified at Neuron construction');
+        process.exit(1);
+      }
+  
+      this.id = id;
+      this.value = value;
+      this.type = type;
+      this.connectors = [];
+    }
+  }
+  
+  module.exports = { Neuron }
