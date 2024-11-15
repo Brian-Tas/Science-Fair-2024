68dab7883f064f1386b636f90a023b3af1c94f9e
Brian-Tas <brianjtascone@gmail.com>
2024-11-15 11:50:30 -0500

    Update Connector.js

--- NeuralNetwork/Connector.js
+++ NeuralNetwork/Connector.js
@@ -1,1 +1,20 @@
+let connectorCounter = 0;
 
+class Connector {
+    constructor(innovation, weight, status) {
+      if(typeof innovation === 'undefined') {
+        console.error("No innovation provided at new connector");
+        process.exit(1);
+      }
+      if(typeof weight === 'undefined') {
+        console.error("No weight provided at new connector");
+        process.exit(1);
+      }
+      this.innovation = innovation;
+      this.weight = weight;
+      this.id = innovation.id;
+      this.status = true;
+    }
+  }
+  
+  module.exports = { Connector };
