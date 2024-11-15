ea8d54540353704c5a65b1965e5e7f31be722d67
Brian-Tas <brianjtascone@gmail.com>
2024-11-15 11:49:47 -0500

    Update Genome.js

--- NeuralNetwork/Genome.js
+++ NeuralNetwork/Genome.js
@@ -1,1 +1,24 @@
-
+class Genome {
+    constructor(neurons = [[],[],[]], weights = [], innovIds = [], order=null) {
+      if(typeof innovIds === 'undefined') {
+        console.log("No innovation id's provided, using default");
+      }
+      if(typeof weights === 'undefined') {
+        console.log("No weights provided, using default");
+      }
+      if(typeof neurons === 'undefined') {
+        console.log("No neurons provided, using default");
+      }
+      if(weights.length !== innovIds.length) {
+        console.error("Innovation Id's aren't matched to innovation weights at Genome");
+        process.exit(1);
+      }
+  
+      this.ids = innovIds;
+      this.weights = weights;
+      this.neurons = neurons;
+      this.order = order;
+    }
+  }
+  
+  module.exports = { Genome }
