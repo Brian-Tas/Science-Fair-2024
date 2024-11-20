744ae2aae89d97ab1c3c2e08020c82a05998bf3b
Brian-Tas <brianjtascone@gmail.com>
2024-11-20 16:40:59 +0000

    added

--- Operations/Mutate.js
+++ Operations/Mutate.js
@@ -1,17 +1,6 @@
 const settings = require("../Storage/Settings.json");
-const { Genome } = require("../NeuralNetwork/Genome");
 
-const mutate = genome => {
-    const neurons = [];
-
-    genome.forEach(layer => {
-        layer.forEach(neuron => {
-            neurons.push(neuron);
-        });
-    });
-
-    // New connections
-
+const mutate = network => {
 
 }
 
