2e351437613dfbf7984ad95d849f38727d442a12
Brian-Tas <brianjtascone@gmail.com>
2024-11-20 03:27:07 +0000

    Added

--- Algorithm.js
+++ Algorithm.js
@@ -1,1 +1,15 @@
+// Import settings i.e. chances/population/other
 const settings = require("./Storage/Settings.json");
+
+// Import operations- i.e. crossover/mutation
+
+// Import network creator to make networks
+const { toNetwork } = require("./Storage/toNetwork");
+
+const networks = [];
+
+for(let i = 0; i < settings.population; i++) {
+    networks.push(toNetwork("./Networks/Blank21.json"));
+}
+
+console.table(networks.length)
